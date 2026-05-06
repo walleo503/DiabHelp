565a7d4be4340daf3c55dd4920eb7e88ecd7c3ba
walleo503 <walter.linares1@catolica.edu.sv>
2026-05-05 21:45:52 -0600

    Nuevo proyecto

---
+++ app/src/main/java/sv/edu/catolica/diabhelp/Perfil_editar.java
@@ -0,0 +1,17 @@
+package sv.edu.catolica.diabhelp;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Perfil_editar extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_perfil_editar);
+
+    }
+}
