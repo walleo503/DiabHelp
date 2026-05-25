a34e50c4ba28f720ea2a6a5d5a87d9508da0d9a6
walleo503 <walter.linares1@catolica.edu.sv>
2026-05-24 22:23:09 -0600

    Cambios de diseño y cambio a favoritos en vez de informacion

--- app/src/main/java/sv/edu/catolica/diabhelp/Favoritos.java
+++ app/src/main/java/sv/edu/catolica/diabhelp/Favoritos.java
@@ -4,17 +4,14 @@ import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
-public class Imformacionydemas extends AppCompatActivity {
+public class Favoritos extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_imformacionydemas);
+        setContentView(R.layout.activity_favoritos);
 
     }
 }
